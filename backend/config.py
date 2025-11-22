8d89f2d799f977b579ba9704ea8fdc8cc151f5f4
Edwin Ade <70894222+valentino7504@users.noreply.github.com>
2025-11-22 23:24:34 +0100

    config: update config.py

--- backend/config.py
+++ backend/config.py
@@ -9,7 +9,9 @@ from tensorflow.keras.models import load_model
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app,  resources={
+    r"/*": {"origins": "https://malaria-diagnosis.netlify.app"}
+})
 loaded_model = load_model(
     "./mobilenet_model/malaria_diagnosis_mobilenetv3Small.keras"
 )
